c5ac2f1bc66c6d4e99f74e86727e1b4c22e7c023
GaganVashishth <gagan.akrolix@gmail.com>
2023-11-04 10:22:49 +0530

    nothing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,16 @@
-import Header from '@/components/common/Header';
-import './globals.css';
-import { Inter } from 'next/font/google';
-import Footer from '@/components/common/Footer';
-import ReactQueryProvider from '@/components/ReactQueryProvider';
-import { FormProvider } from './context/formContext';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import Header from "@/components/common/Header";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/common/Footer";
+import ReactQueryProvider from "@/components/ReactQueryProvider";
+import { FormProvider } from "./context/formContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'E-Visa',
+  title: "E-Visa",
 };
 
 export default function RootLayout({ children }) {
