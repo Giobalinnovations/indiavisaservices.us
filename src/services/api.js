cf2880fc85e4d8f10405db31ccfd7eb0afb96161
fastestsunil <sunil141292@gmail.com>
2023-11-22 23:17:34 +0530

    base url

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,7 @@ const axiosInstance = axios.create({
   baseURL: 'https://nqcfsdj9u6.us-east-1.awsapprunner.com/',
   // baseURL: 'https://api.github.com/repos/TanStack/',
   // baseURL: 'http://localhost:8090/',
+  //
   timeout: 30000,
 });
 
