a6144a2fdab4e3e8b9dd822dacd8049e6b938d2d
fastestsunil <sunil141292@gmail.com>
2024-06-18 10:40:37 +0530

    api add

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://nqcfsdj9u6.us-east-1.awsapprunner.com/',
-  // baseURL: 'https://api.github.com/repos/TanStack/',
+  baseURL: 'https://b6zsgkcqfp.us-east-1.awsapprunner.com/',
   // baseURL: 'http://localhost:8090/',
   timeout: 30000,
 });
