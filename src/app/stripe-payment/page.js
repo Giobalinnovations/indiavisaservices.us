12bd54ac88a233e45f58df8d7e51527e4d44289b
fastestsunil <sunil141292@gmail.com>
2024-07-02 12:42:41 +0530

    add payment key

---
+++ src/app/stripe-payment/page.js
@@ -0,0 +1,42 @@
+'use client';
+import useVisaBookingPaymentPost from '@/hooks/useVisaBookingPaymentPost';
+import { loadStripe } from '@stripe/stripe-js';
+import { useEffect } from 'react';
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST
+);
+export default function StripePaymentTesting() {
+  const postPayment = useVisaBookingPaymentPost({
+    apiEndpointUrl: 'api/checkout-session/evisa9503592',
+    successMessage: 'Successful',
+  });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    postPayment.mutate({});
+  };
+
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get('success')) {
+      console.log('Order placed! You will receive an email confirmation.');
+    }
+
+    if (query.get('canceled')) {
+      console.log(
+        'Order canceled -- continue to shop around and checkout when you’re ready.'
+      );
+    }
+  }, []);
+  return (
+    <div className="px-10 py-40">
+      <form onSubmit={handleSubmit}>
+        <button className="text-white bg-primary">
+          {postPayment.isPending ? 'Loading...' : 'Make Payment'}
+        </button>
+      </form>
+    </div>
+  );
+}
