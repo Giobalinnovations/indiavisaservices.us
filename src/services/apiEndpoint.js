9412222c31e85d88485d6d3a945c4532907c9c9a
fastestsunil <sunil141292@gmail.com>
2024-07-05 16:23:47 +0530

    updated env

--- src/services/apiEndpoint.js
+++ src/services/apiEndpoint.js
@@ -1,39 +1,39 @@
 const apiEndpoint = {
   // India endpoint
-  EVISA_USER_LOGIN: '/visa/services-travel-india/loginEvisaUser',
-  VISA_ADD_STEP1: '/visa/services-travel-india/add/step-one',
-  UPDATE_VISA_ADD_STEP1: '/visa/services-travel-india/viewVisaRequestForm',
+  EVISA_USER_LOGIN: '/visa/travel-to-india-services/loginEvisaUser',
+  VISA_ADD_STEP1: '/visa/travel-to-india-services/add/step-one',
+  UPDATE_VISA_ADD_STEP1: '/visa/travel-to-india-services/viewVisaRequestForm',
   UPDATE_VISA_ADD_STEP1_LAST_EXIT_STEP_URL:
-    '/visa/services-travel-india/viewVisaRequestFormLastExitStepUrl',
+    '/visa/travel-to-india-services/viewVisaRequestFormLastExitStepUrl',
 
   UPDATE_VISA_FORM_PAYMENT:
-    '/visa/services-travel-india/viewVisaRequestFormPayment',
+    '/visa/travel-to-india-services/viewVisaRequestFormPayment',
 
-  GET_VISA_STEP1_BY_ID: '/visa/services-travel-india/viewVisaRequestForm/',
-  VISA_ADD_STEP2: '/visa/services-travel-india/add/step-two',
-  UPDATE_VISA_ADD_STEP2: '/visa/services-travel-india/viewVisaRequestForm2',
+  GET_VISA_STEP1_BY_ID: '/visa/travel-to-india-services/viewVisaRequestForm/',
+  VISA_ADD_STEP2: '/visa/travel-to-india-services/add/step-two',
+  UPDATE_VISA_ADD_STEP2: '/visa/travel-to-india-services/viewVisaRequestForm2',
   GET_VISA_STEP2_BY_FORM_ID:
-    '/visa/services-travel-india/viewVisaRequestForm2ByFormId/',
-  VISA_ADD_STEP3: '/visa/services-travel-india/add/step-three',
-  UPDATE_VISA_ADD_STEP3: '/visa/services-travel-india/viewVisaRequestForm3',
-  VISA_ADD_STEP4: '/visa/services-travel-india/add/step-four',
-  UPDATE_VISA_ADD_STEP4: '/visa/services-travel-india/viewVisaRequestForm4',
-  VISA_ADD_STEP5: '/visa/services-travel-india/add/step-five',
-  UPDATE_VISA_ADD_STEP5: '/visa/services-travel-india/viewVisaRequestForm5',
-  VISA_ADD_STEP6: '/visa/services-travel-india/add/step-six',
-  VISA_ADD_STEP8: '/visa/services-travel-india/add/step-eight',
+    '/visa/travel-to-india-services/viewVisaRequestForm2ByFormId/',
+  VISA_ADD_STEP3: '/visa/travel-to-india-services/add/step-three',
+  UPDATE_VISA_ADD_STEP3: '/visa/travel-to-india-services/viewVisaRequestForm3',
+  VISA_ADD_STEP4: '/visa/travel-to-india-services/add/step-four',
+  UPDATE_VISA_ADD_STEP4: '/visa/travel-to-india-services/viewVisaRequestForm4',
+  VISA_ADD_STEP5: '/visa/travel-to-india-services/add/step-five',
+  UPDATE_VISA_ADD_STEP5: '/visa/travel-to-india-services/viewVisaRequestForm5',
+  VISA_ADD_STEP6: '/visa/travel-to-india-services/add/step-six',
+  VISA_ADD_STEP8: '/visa/travel-to-india-services/add/step-eight',
   GET_VISA_STEP8_BY_FORM_ID:
-    '/visa/services-travel-india/viewVisaRequestForm8ByFormId/',
-  GET_ALL_STEPS_DATA: '/visa/services-travel-india/getAllStepData/',
+    '/visa/travel-to-india-services/viewVisaRequestForm8ByFormId/',
+  GET_ALL_STEPS_DATA: '/visa/travel-to-india-services/getAllStepData/',
 
   // temporary exit
   VISA_ADD_TEMPORARY_EXIT:
-    '/visa/services-travel-india/visaLastTemporaryExitUrl',
+    '/visa/travel-to-india-services/visaLastTemporaryExitUrl',
   VISA_GET_TEMPORARY_EXIT_BY_FORM_ID:
-    '/visa/services-travel-india/visaLastTemporaryExitUrl/',
+    '/visa/travel-to-india-services/visaLastTemporaryExitUrl/',
 
   // payment routes
-  INDIA_VISA_PAYMENT: '/visa/services-travel-india/indiaVisa',
+  INDIA_VISA_PAYMENT: '/visa/travel-to-india-services/indiaVisa',
 };
 
 export default apiEndpoint;
